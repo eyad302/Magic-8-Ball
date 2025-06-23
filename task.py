938408afc363d7b279b1e52ccf0456b0031a6460
eyad302 <eyadazab45@gmail.com>
2025-06-23 21:16:59 +0300

    random

---
+++ task.py
@@ -0,0 +1,24 @@
+import random
+
+reponses=[
+    "yes,definitly!",
+    "no,not now.",
+    "Ask again later",
+    "it is certien",
+    "very doubtful"
+    "outlook is good",
+    "better not tell you now",
+    "comcentrarte and ask again"
+]
+def get_random():
+    return random.choice(reponses)
+def get_user_guess():
+    while True:
+        try:
+            guess=int(input("Enter your guess (1-100)"))
+            if 1<=guess<=100:
+                return guess
+            else:
+                print("pls enter a num between 1 and 100")
+        except ValueError:
+            print("invalid input")
